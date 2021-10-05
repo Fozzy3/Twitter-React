62fd69debe33940d92ec18be96a53fc984a3cd64
Nicolas Mayorga <namayorgav@gmail.com>
2021-10-04 20:23:24 -0500

    Prueba correcta con un tweet desde la API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import Home from './components/Home';
-import Noticias from './components/Noticias';
-import Principal from './components/Principal';
-
+import "./App.css";
+import Home from "./components/Home";
+import Principal from "./components/Principal";
+import Tweets from "./components/Test";
 
 function App() {
   return (
-        <div className="container">
-          <Home/>
-          <Noticias/>
-          <Principal/>
-        </div>
+    <div className="container">
+      <Home />
+      <Tweets />
+      <Principal />
+    </div>
   );
 }
 
